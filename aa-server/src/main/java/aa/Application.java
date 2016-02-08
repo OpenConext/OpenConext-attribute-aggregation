ca9c21ebeacf3c0d50d0cf918a67a383b9c74d72
oharsta <oharsta@zilverline.com>
2016-02-08 16:47:11 +0100

    Do not add not used debug metrics

--- aa-server/src/main/java/aa/Application.java
+++ aa-server/src/main/java/aa/Application.java
@@ -1,9 +1,11 @@
 package aa;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.MetricFilterAutoConfiguration;
+import org.springframework.boot.actuate.autoconfigure.TraceWebFilterAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
+@SpringBootApplication(exclude = {TraceWebFilterAutoConfiguration.class, MetricFilterAutoConfiguration.class})
 public class Application {
 
   public static void main(String[] args) {
