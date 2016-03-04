375771733b4aae4337a6ff1dc37a72a3f62fc801
oharsta <oharsta@zilverline.com>
2016-03-04 20:50:24 +0100

    Added sorting to metadata fetch

--- aa-server/src/test/java/aa/serviceregistry/UrlResourceServiceRegistryTest.java
+++ aa-server/src/test/java/aa/serviceregistry/UrlResourceServiceRegistryTest.java
@@ -1,5 +1,6 @@
 package aa.serviceregistry;
 
+import aa.model.ServiceProvider;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import org.apache.commons.io.IOUtils;
 import org.junit.Before;
@@ -8,6 +9,9 @@ import org.junit.Test;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static junit.framework.TestCase.assertEquals;
@@ -22,12 +26,7 @@ public class UrlResourceServiceRegistryTest {
 
   @Before
   public void before() throws IOException {
-    String spResponse = IOUtils.toString(new ClassPathResource("service-registry/service-providers.json").getInputStream());
-    stubFor(get(urlEqualTo("/sp")).willReturn(aResponse().withStatus(200).withHeader("Content-Type", "application/json").withBody(spResponse)));
-
-    stubFor(head(urlEqualTo("/sp")).withHeader(IF_MODIFIED_SINCE, notMatching("X")).willReturn(aResponse().withStatus(200)));
-
-    this.subject = (UrlResourceServiceRegistry) new ServiceRegistryConfiguration().urlResourceServiceRegistry("user","password","http://localhost:8889/sp", 10);
+    doBefore("service-registry/service-providers.json");
   }
 
   @Test
@@ -41,4 +40,24 @@ public class UrlResourceServiceRegistryTest {
     stubFor(head(urlEqualTo("/sp")).withHeader(IF_MODIFIED_SINCE, notMatching("X")).willReturn(aResponse().withStatus(304)));
     subject.initializeMetadata();
   }
+
+  @Test
+  public void testSorting() throws IOException {
+    doBefore("service-registry-test/service-providers.json");
+    List<ServiceProvider> serviceProviders = new ArrayList<>(subject.serviceProviders());
+
+    assertEquals("Bas Test-SP", serviceProviders.get(0).getName());
+    assertEquals("OpenConext PDP", serviceProviders.get(serviceProviders.size() - 1).getName());
+
+  }
+
+  private void doBefore(String path) throws IOException {
+    String spResponse = IOUtils.toString(new ClassPathResource(path).getInputStream());
+    stubFor(get(urlEqualTo("/sp")).willReturn(aResponse().withStatus(200).withHeader("Content-Type", "application/json").withBody(spResponse)));
+
+    stubFor(head(urlEqualTo("/sp")).withHeader(IF_MODIFIED_SINCE, notMatching("X")).willReturn(aResponse().withStatus(200)));
+
+    this.subject = (UrlResourceServiceRegistry) new ServiceRegistryConfiguration().urlResourceServiceRegistry("user","password","http://localhost:8889/sp", 10);
+  }
+
 }
