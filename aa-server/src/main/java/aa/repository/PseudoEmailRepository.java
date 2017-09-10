040c4ded13e3cd1b2a0a2e8c33afef8ae333a2fe
oharsta <oharsta@zilverline.com>
2017-09-10 15:13:24 +0200

    Organized imports

--- aa-server/src/main/java/aa/repository/PseudoEmailRepository.java
+++ aa-server/src/main/java/aa/repository/PseudoEmailRepository.java
@@ -1,12 +1,9 @@
 package aa.repository;
 
-import aa.model.Account;
-import aa.model.AccountType;
 import aa.model.PseudoEmail;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
